816d39b31f1c42bd8bae315ce368d85d32c11c61
daichi_sato <daichiexperience@gmail.com>
2022-01-05 22:25:20 +0900

    first commit

---
+++ components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <header>
+    <div class="container">
+      <div class="logo">
+        <img src="/assets/img/logo_sticky.png" alt="" />
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <a href="">PIX TECHの特徴</a>
+          </li>
+          <li>
+            <a href="">PIX TECHの思い</a>
+          </li>
+          <li>
+            <a href="">カリキュラム</a>
+          </li>
+          <li>
+            <a href="">コースの紹介</a>
+          </li>
+          <li>
+            <a href="">講師の紹介</a>
+          </li>
+          <li>
+            <a href="">よくある質問</a>
+          </li>
+          <li>
+            <a href="">説明会へ申し込む</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+</template>
