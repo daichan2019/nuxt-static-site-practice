6effc11680db07888dfe79bbfe1e993eb5a4a82e
daichi_sato <daichiexperience@gmail.com>
2022-01-05 22:35:55 +0900

    add assets dir

--- components/Header.vue
+++ components/Header.vue
@@ -5,26 +5,26 @@
         <img src="/assets/img/logo_sticky.png" alt="" />
       </div>
       <nav>
-        <ul>
-          <li>
+        <ul class="nav-list">
+          <li class="nav-item">
             <a href="">PIX TECHの特徴</a>
           </li>
-          <li>
+          <li class="nav-item">
             <a href="">PIX TECHの思い</a>
           </li>
-          <li>
+          <li class="nav-item">
             <a href="">カリキュラム</a>
           </li>
-          <li>
+          <li class="nav-item">
             <a href="">コースの紹介</a>
           </li>
-          <li>
+          <li class="nav-item">
             <a href="">講師の紹介</a>
           </li>
-          <li>
+          <li class="nav-item">
             <a href="">よくある質問</a>
           </li>
-          <li>
+          <li class="nav-item">
             <a href="">説明会へ申し込む</a>
           </li>
         </ul>
@@ -32,3 +32,22 @@
     </div>
   </header>
 </template>
+
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.nav-list {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.nav-item {
+  & + & {
+    margin-left: 20px;
+  }
+}
+</style>
